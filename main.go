a1525b6e65bc439d53f7e35840af113df90e9015
danielb <git@masterunit.net>
2020-10-28 00:08:22 +0100

    hash concurrently

--- main.go
+++ main.go
@@ -5,44 +5,77 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/minio/highwayhash"
 )
 
+var (
+	rc  int
+	key = []byte("12345678901234567890123456789012")
+	wg  = new(sync.WaitGroup)
+)
+
 func main() {
+
 	log.SetOutput(os.Stdout)
 	log.SetFlags(0)
 
-	if len(os.Args) != 2 {
-		log.Fatalln("usage: highwayhashsum <file>")
+	if len(os.Args) < 2 {
+		log.Fatalln("usage: highwayhashsum <file> [<file> ...]")
 	}
 
-	filename := os.Args[1]
+	filenames := os.Args[1:]
 
-	fileinfo, err := os.Stat(filename)
-	if err != nil {
-		log.Fatalln(err)
+	for _, filename := range filenames {
+		fileinfo, err := os.Stat(filename)
+		if err != nil {
+			log.Println(err)
+			rc = 1
+			continue
+		}
+
+		if !fileinfo.Mode().IsRegular() {
+			log.Println("Not a regular file: ", filename)
+			rc = 1
+			continue
+		}
+
+		wg.Add(1)
+		go hash(filename)
 	}
-	if !fileinfo.Mode().IsRegular() {
-		log.Fatalln("Not a regular file: ", filename)
-	}
+
+	wg.Wait()
+	os.Exit(rc)
+}
+
+func hash(filename string) {
 
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Fatalln("Failed to open file: ", err.Error())
+		log.Println("Failed to open file: ", err.Error())
+		rc = 1
+		wg.Done()
+		return
 	}
 	defer file.Close()
 
-	key := []byte("12345678901234567890123456789012")
 	hash, err := highwayhash.New(key)
 	if err != nil {
-		log.Fatalln("Failed to create HighwayHash instance: ", err.Error())
+		log.Println("Failed to create HighwayHash instance: ", err.Error())
+		rc = 1
+		wg.Done()
+		return
 	}
 
 	if _, err = io.Copy(hash, file); err != nil {
-		log.Fatalln("Failed to read from file: ", err.Error())
+		log.Println("Failed to read from file: ", err.Error())
+		rc = 1
+		wg.Done()
+		return
 	}
 
 	checksum := hash.Sum(nil)
 	log.Printf("%v  %v\n", hex.EncodeToString(checksum), filename)
+	wg.Done()
 }
